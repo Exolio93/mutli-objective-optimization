dd36a69f88fddcebe2f286bc46287e11307028c6
Exolio93 <exolio93.teix@gmail.com>
2024-05-31 10:35:51 +0200

    reset

--- src/tools.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/tools.hpp"
-
-void print_and_exit(const std::string& message) {
-    std::cerr << "///// ERREUR /////" << std::endl;
-    std::cerr << message << std::endl;
-    std::cerr << "//////////////////" << std::endl;
-    std::exit(EXIT_FAILURE);
-}
